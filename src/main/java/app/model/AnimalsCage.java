3ef83892a492ccadc42b971ed67172f290b0ae3b
DragonaC.B <Dragona163@bk.ru>
2025-04-02 01:32:10 +0500

    first commit

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,12 +1,21 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    private Timer timer;
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    @Qualifier("beanDog")
+    @Autowired()
     private Animal animal;
 
     public void whatAnimalSay() {
